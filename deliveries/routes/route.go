c9c79c0ecd3efabad73bd6d7791e64d89e30566d
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-25 07:00:32 +0700

    routing room and image

--- deliveries/routes/route.go
+++ deliveries/routes/route.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"group-project2/deliveries/controllers/auth"
+	"group-project2/deliveries/controllers/image"
+	"group-project2/deliveries/controllers/room"
 	"group-project2/deliveries/controllers/user"
 	"group-project2/deliveries/middlewares"
 
@@ -9,7 +11,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func RegisterPaths(e *echo.Echo, ac *auth.AuthController, uc *user.UserController) {
+func RegisterPaths(e *echo.Echo, ac *auth.AuthController, uc *user.UserController, rc *room.RoomController, ic *image.ImageController) {
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.CORS())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
@@ -21,9 +23,31 @@ func RegisterPaths(e *echo.Echo, ac *auth.AuthController, uc *user.UserControlle
 
 	ur := e.Group("/users")
 	ur.POST("", uc.Insert())
-	ur.GET("", uc.GetUserByID(), middlewares.JWTMiddleware())
-	ur.PUT("", uc.Update(), middlewares.JWTMiddleware())
-	ur.PUT("/setrenter", uc.SetRenter(), middlewares.JWTMiddleware())
-	ur.DELETE("", uc.DeleteByID(), middlewares.JWTMiddleware())
+	urj := ur.Group("/jwt")
+	urj.Use(middlewares.JWTMiddleware())
+	urj.GET("", uc.GetUserByID())
+	urj.PUT("", uc.Update())
+	urj.PUT("/setrenter", uc.SetRenter())
+	urj.DELETE("", uc.DeleteByID())
 
+	rr := e.Group("/rooms")
+	rr.GET("", rc.GetAllRooms())
+	rr.GET("/:id", rc.GetRoomByID())
+	rr.GET("", rc.GetRoomsByCity())
+	rrj := rr.Group("/jwt")
+	rrj.Use(middlewares.JWTMiddleware())
+	rrj.POST("", rc.Insert())
+	rrj.GET("", rc.GetRoomsByUserID())
+	rrj.PUT("/:id", rc.Update())
+	rrj.DELETE("/:id", rc.Delete())
+
+	ir := e.Group("/images")
+	ir.GET("", ic.GetImagesByRoomID())
+	ir.GET("/:id", ic.GetImageByID())
+	irj := ir.Group("/jwt")
+	irj.Use(middlewares.JWTMiddleware())
+	irj.POST("", ic.Insert())
+	irj.PUT("", ic.Update())
+	irj.DELETE("/:id", ic.DeleteImageByID())
+	irj.DELETE("", ic.DeleteImageByRoomID())
 }

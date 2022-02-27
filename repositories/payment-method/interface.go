a09110bb52815832ae0ff18a332c9a900c96bf19
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-27 20:11:14 +0700

    payment method repository done

--- repositories/payment-method/interface.go
+++ repositories/payment-method/interface.go
@@ -1,1 +1,9 @@
 package paymentmethod
+
+import P "group-project2/entities/payment-method"
+
+type PaymentMethod interface {
+	Insert(NewPaymentMethod P.PaymentMethods) (P.PaymentMethods, error)
+	Get() ([]P.PaymentMethods, error)
+	Delete(ID int) error
+}
--- repositories/payment-method/payment-method.go
+++ repositories/payment-method/payment-method.go
@@ -1,1 +1,43 @@
 package paymentmethod
+
+import (
+	"errors"
+	P "group-project2/entities/payment-method"
+
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+type PaymentMethodRepository struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) *PaymentMethodRepository {
+	return &PaymentMethodRepository{
+		db: db,
+	}
+}
+
+func (repo *PaymentMethodRepository) Insert(NewPaymentMethod P.PaymentMethods) (P.PaymentMethods, error) {
+	if err := repo.db.Create(&NewPaymentMethod).Error; err != nil {
+		log.Warn(err)
+		return P.PaymentMethods{}, err
+	}
+	return NewPaymentMethod, nil
+}
+
+func (repo *PaymentMethodRepository) Get() ([]P.PaymentMethods, error) {
+	paymentMethods := []P.PaymentMethods{}
+	if RowsAffected := repo.db.Find(&paymentMethods).RowsAffected; RowsAffected == 0 {
+		return nil, errors.New("belum ada payment method yang terdaftar")
+	}
+	return paymentMethods, nil
+}
+
+func (ur *PaymentMethodRepository) Delete(ID int) error {
+	paymentMethod := P.PaymentMethods{}
+	if RowsAffected := ur.db.Delete(&paymentMethod, ID).RowsAffected; RowsAffected == 0 {
+		return errors.New("tidak ada payment method yang dihapus")
+	}
+	return nil
+}

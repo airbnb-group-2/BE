ff9aa633069ca6857f6a6956eeed3b7c385e1e6e
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-27 20:13:08 +0700

    add gorm tag

--- entities/book/book.go
+++ entities/book/book.go
@@ -8,11 +8,11 @@ import (
 
 type Books struct {
 	gorm.Model
-	Status          string `gorm:"type:enum(booked,paid,cancel)"`
-	Phone           string `gorm:"type:varchar(14)"`
 	CheckIn         time.Time
 	CheckOut        time.Time
-	UserID          uint
-	RoomID          uint
-	PaymentMethodID uint
+	Status          string `gorm:"type:enum(booked,paid,cancel)"`
+	Phone           string `gorm:"type:varchar(14)"`
+	UserID          uint   `gorm:"not null"`
+	RoomID          uint   `gorm:"not null"`
+	PaymentMethodID uint   `gorm:"not null"`
 }

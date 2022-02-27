1aafea2c374dbd9f15d13ea54397722a66e12761
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-27 22:06:07 +0700

    book repository done

--- repositories/book/book.go
+++ repositories/book/book.go
@@ -3,6 +3,7 @@ package book
 import (
 	"errors"
 	B "group-project2/entities/book"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -38,6 +39,24 @@ func (repo *BookRepository) SetCancel(BookID uint) (B.Books, error) {
 	Book := B.Books{}
 	if RowsAffected := repo.db.Table("books").Where("id = ?", BookID).Update("status", "cancel").RowsAffected; RowsAffected == 0 {
 		return B.Books{}, errors.New("gagal mengubah status booking menjadi cancel")
+	}
+	repo.db.First(&Book, BookID)
+	return Book, nil
+}
+
+func (repo *BookRepository) SetCheckInTime(BookID uint, CheckInTime time.Time) (B.Books, error) {
+	Book := B.Books{}
+	if RowsAffected := repo.db.Table("books").Where("id = ?", BookID).Update("check_in_time", CheckInTime).RowsAffected; RowsAffected == 0 {
+		return B.Books{}, errors.New("gagal mengatur check_in_time")
+	}
+	repo.db.First(&Book, BookID)
+	return Book, nil
+}
+
+func (repo *BookRepository) SetCheckOutTime(BookID uint, CheckOutTime time.Time) (B.Books, error) {
+	Book := B.Books{}
+	if RowsAffected := repo.db.Table("books").Where("id = ?", BookID).Update("check_out_time", CheckOutTime).RowsAffected; RowsAffected == 0 {
+		return B.Books{}, errors.New("gagal mengatur check_out_time")
 	}
 	repo.db.First(&Book, BookID)
 	return Book, nil
--- repositories/book/interface.go
+++ repositories/book/interface.go
@@ -1,9 +1,14 @@
 package book
 
-import B "group-project2/entities/book"
+import (
+	B "group-project2/entities/book"
+	"time"
+)
 
 type Book interface {
 	Insert(NewBook B.Books) (B.Books, error)
 	SetPaid(BookID uint) (B.Books, error)
 	SetCancel(BookID uint) (B.Books, error)
+	SetCheckInTime(BookID uint, CheckInTime time.Time) (B.Books, error)
+	SetCheckOutTime(BookID uint, CheckOutTime time.Time) (B.Books, error)
 }

127c7c26fcab48a90103c22b427bf11f4823c1a9
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-27 21:20:21 +0700

    add checkin/out time

--- entities/book/book.go
+++ entities/book/book.go
@@ -10,6 +10,8 @@ type Books struct {
 	gorm.Model
 	CheckIn         time.Time
 	CheckOut        time.Time
+	CheckInTime     time.Time
+	CheckOutTime    time.Time
 	Status          string `gorm:"type:enum(booked,paid,cancel)"`
 	Phone           string `gorm:"type:varchar(14)"`
 	UserID          uint   `gorm:"not null"`
